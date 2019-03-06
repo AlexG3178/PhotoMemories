54196f433177a0280f6886a1f3fb1ad0007baed6
alexg3178 <alexg3178@gmail.com>
2019-03-06 21:09:42 +0200

    Initial Commit

---
+++ PhotoMemories/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhotoMemories
+//
+//  Created by Alexandr Grigoriev on 3/6/19.
+//  Copyright © 2019 AlexG. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
